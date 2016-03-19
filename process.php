5db76ce747c9d2f228fc46ca070d3e1651f40e2b
Sharanc25 <Sharanc25@gmail.com>
2016-03-19 20:21:12 +0530

    Updates process.php file

--- process.php
+++ process.php
@@ -6,13 +6,27 @@ use GuzzleHttp\Client;
 use Ivory\HttpAdapter\Guzzle6HttpAdapter;
 
 $httpAdapter = new Guzzle6HttpAdapter(new Client());
-$sparky = new SparkPost($httpAdapter, ['key'=>getEnv('9ba9a7df02202d283455f92f2710d2b81b40cb9b')]);
+$sparky = new SparkPost($httpAdapter, ['key'=>'YOUR API KEY']);
 
+try {
+// Build your email and send it!
 $results = $sparky->transmission->send([
-    'from'=>'sharanc25@gmail.com',
-    'html'=>'<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p></body></html>',
-    'subject'=> 'Oh hey!',
-    'recipients'=>[
-        ['address'=>['email'=>'sharanc25@gmail.com']]
-    ]
+'from'=>'From Envelope <from@sparkpostbox.com>',
+'html'=>'<html><body><h1>Congratulations, {{name}}!</h1><p>You just sent your very first mailing!</p></body></html>',
+'text'=>'Congratulations, {{name}}!! You just sent your very first mailing!',
+'substitutionData'=>['name'=>'YOUR FIRST NAME'],
+'subject'=>'First Mailing From PHP',
+'recipients'=>[
+[
+'address'=>[
+'name'=>'YOUR FULL NAME',
+'email'=>'YOUR EMAIL ADDRESS'
+]
+]
+]
 ]);
+echo 'Woohoo! You just sent your first mailing!';
+} catch (\Exception $err) {
+echo 'Whoops! Something went wrong';
+var_dump($err);
+}
