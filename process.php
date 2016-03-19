d54c0f9ab507658c99019e77a42fbdd39a2bde3f
Sharanc25 <Sharanc25@gmail.com>
2016-03-19 23:31:26 +0530

    Adds email address

--- process.php
+++ process.php
@@ -9,22 +9,20 @@ $httpAdapter = new Guzzle6HttpAdapter(new Client());
 $sparky = new SparkPost($httpAdapter, ['key'=>'faa9dd73426b59b7b84d7331fcaa870bf0dc6907']);
 
 try {
-// Build your email and send it!
-$results = $sparky->transmission->send([
-'from'=>'sharanc25@gmail.com',
-'html'=>'<html><body><h1>Congratulations,!</h1><p>You just sent your very first mailing!</p></body></html>',
-'text'=>'Congratulations,!! You just sent your very first mailing!',
-'substitutionData'=>['name'=>'YOUR FIRST NAME'],
-'subject'=>'First Mailing From PHP',
-'recipients'=>[
-[
-'address'=>[
-'name'=>'Sharan',
-'email'=>'Sharanc25@gmail.com'
-]
-]
-]
-]);
+  $results = $sparky->transmission->send([
+    'from'=>'admin@sappcreation.com',
+    'html'=>'<p>Hello World!</p>',
+    'text'=>'Hello World!',
+    'subject'=>'Example Email',
+    'recipients'=>[
+      [
+        'address'=>[
+          'email'=>'sharanc25@gmail.com'
+        ]
+      ]
+    ]
+  ]);
+
 echo 'Woohoo! You just sent your first mailing!';
 } catch (\Exception $err) {
 echo 'Whoops! Something went wrong';
